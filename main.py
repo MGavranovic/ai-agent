eafe986f81de292882666f7386e964fd13db6fad
Milos Gavranovic <mgavranovic7@gmail.com>
2025-06-16 10:53:25 +0200

    Add prompt as a cmd arg

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai.errors import ServerError
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-prompt = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-response = prompt.text
+script_args = sys.argv
+if len(script_args) < 2:
+    print("Prompt hasn't been provided as an arg for python3 command")
+    print("Please make sure to use python3 main.py [prompt]")
+    sys.exit(1)
 
-print(response)
-print("Prompt tokents: ", prompt.usage_metadata.prompt_token_count)
-print("Response tokents: ", prompt.usage_metadata.candidates_token_count)
+prompt_arg = script_args[1]
+# print(script_args)
+try:
+    api_response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt_arg)
+    api_response_text = api_response.text
+
+    print(api_response_text)
+    print("Prompt tokens:", api_response.usage_metadata.prompt_token_count)
+    print("Response tokens:", api_response.usage_metadata.candidates_token_count)
+except ServerError:
+    print("The Gemini API is temporarily unavailable. Please try again later.")
