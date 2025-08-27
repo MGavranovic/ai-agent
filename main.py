e4b508c959668bf76dd976c2010e9e6b0ccfbe2a
Milos Gavranovic <mgavranovic7@gmail.com>
2025-08-27 11:02:25 +0200

    Add system prompt and toml

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+
 script_args = sys.argv
 if len(script_args) < 2:
     print("Prompt hasn't been provided as an arg for python3 command")
@@ -19,7 +21,7 @@ if len(script_args) < 2:
 prompt_arg = script_args[1]
 messages = [types.Content(role="user", parts=[types.Part(text=prompt_arg)])]
 try:
-    api_response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    api_response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages,config=types.GenerateContentConfig(system_instruction=system_prompt))
     api_response_text = api_response.text
 
     if "--verbose" in script_args:
